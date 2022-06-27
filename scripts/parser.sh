b31f25451d7f339f51f8afe99005e561a1c446fa
zhijli <307999258@qq.com>
2022-06-27 23:11:22 +0800

    增加解析器

--- scripts/parser.sh
+++ scripts/parser.sh
@@ -22,7 +22,7 @@ function fbfu_line_foreach {
         return 1
     fi
     while read fbar_line;do
-        fbar_line=$(fbfu_slim "$fbar_line")
+        fbar_line=$(fbfu_string_slim "$fbar_line")
         if [ "$fbar_line" == "" ];then
             continue
         fi
@@ -33,6 +33,141 @@ function fbfu_line_foreach {
         fi
     done < $fbar_file
     return 0
+}
+
+function fbfr_section {
+    local fbar_i=1
+    local fbar_file=$1
+    local fbar_key=$2
+    local fbar_section_left=$3
+    local fbar_section_right=$4
+    local fbar_nsl=0
+    local fbar_nsr=0
+    local fbar_sl_sum=0
+    local fbar_sr_sum=0
+    local fbar_value=""
+    local fbar_line=""
+    local fbar_stage=0
+    local fbar_section_begin=0
+    local fbar_section_end=0
+    while read fbar_line;do
+        fbar_line=$(fbfu_string_slim "$fbar_line")
+        if [ "$fbar_line" == "" ];then
+            fbar_i=$[ $fbar_i + 1 ]
+            continue
+        fi
+        if [ "$fbar_stage" == "0" ];then
+            local fbar_check_key=$(echo "$fbar_line" | grep "^${fbar_key}[[:space:]]*=")
+            if [ "$fbar_check_key" == "" ];then
+                fbar_i=$[ $fbar_i + 1 ]
+                continue
+            fi
+            fbar_stage=1
+            fbar_value=$(echo "$fbar_line" | sed -e "s/^${fbar_key}[[:space:]]*=[[:space:]]*\(.*\)/\1/")
+            fbar_check_key=$(echo "$fbar_value" | grep "^\\${fbar_section_left}")
+            if [ "$fbar_check_key" == "" ];then
+                return 1
+            fi
+            fbar_section_begin=$fbar_i
+            fbar_nsl=$(fbfu_string_symbol_num "$fbar_line" "$fbar_section_left")
+            fbar_nsr=$(fbfu_string_symbol_num "$fbar_line" "$fbar_section_right")
+            fbar_sl_sum=$[ "$fbar_sl_sum" + "$fbar_nsl" ]
+            fbar_sr_sum=$[ "$fbar_sr_sum" + "$fbar_nsr" ]
+            if [ "$fbar_sl_sum" == "$fbar_sr_sum" ];then
+                fbar_section_end=$fbar_i
+                break
+            fi
+            fbar_i=$[ $fbar_i + 1 ]
+            continue
+        elif [ "$fbar_stage" == "1" ];then
+            fbar_nsl=$(fbfu_string_symbol_num "$fbar_line" "$fbar_section_left")
+            fbar_nsr=$(fbfu_string_symbol_num "$fbar_line" "$fbar_section_right")
+            fbar_sl_sum=$[ "$fbar_sl_sum" + "$fbar_nsl" ]
+            fbar_sr_sum=$[ "$fbar_sr_sum" + "$fbar_nsr" ]
+            if [ "$fbar_sl_sum" == "$fbar_sr_sum" ];then
+                fbar_section_end=$fbar_i
+                break
+            fi
+        fi
+        fbar_i=$[ $fbar_i + 1 ]
+    done < $fbar_file
+    if [ "$fbar_section_begin" == "0" ] || [ "$fbar_section_end" == "0" ] || [ "$fbar_sl_sum" != "$fbar_sr_sum" ];then
+        return 1
+    fi
+    sed -n "${fbar_section_begin},${fbar_section_end}p" "$fbar_file" | sed -e "s/^${fbar_key}[[:space:]]*=[[:space:]]*//"
+    return 0
+}
+
+function fbfr_parse_kv {
+    local fbar_file=$1
+    local fbar_key=$2
+    local fbar_symbol=$3
+    local fbar_line=$(cat $fbar_file | grep "^$fbar_key")
+    if [ "$fbar_line" == "" ];then
+        return 1
+    fi
+    local fbar_value=$(echo "$fbar_line" | sed -e "s/^${fbar_key}[[:space:]]*${fbar_symbol}[[:space:]]*//" | sed -n '1p')
+    if [ "$fbar_value" == "$fbar_line" ];then
+        return 1
+    fi
+    fbar_value=$(fbfu_string_slim "$fbar_value")    
+    echo "$fbar_value"
+    return 0
+}
+
+function fbfr_parse_hook {
+    local fbar_command=$1
+    local fbar_temp_path=$2
+    if [ -f "${fbar_temp_path}/__hook_sh" ];then
+        rm -f ${fbar_temp_path}/__hook_sh
+    fi
+    mkdir -p ${fbar_temp_path}
+    echo "$fbar_command" > ${fbar_temp_path}/__hook_sh
+    chmod 755 ${fbar_temp_path}/__hook_sh
+    ${fbar_temp_path}/__hook_sh
+}
+
+#@brief fb配置解析接口
+#@param 文件路径
+#@param 需要解析的key值
+#@return 若解析成功，返回解析的字符串
+function fbfu_parse {
+    local fbar_file=$1
+    local fbar_key=$2
+    local fbar_temp_path=$3
+    local fbar_check_section=""
+    local fbar_command=""
+    if [ ! -f "$fbar_file" ] || [ "$fbar_key" == "" ];then
+        return 0;
+    fi
+    local fbar_value=$(fbfr_parse_kv "$fbar_file" "$fbar_key" "=")
+    if [ "$fbar_value" == "" ];then
+        return 0
+    fi
+    fbar_check_section=$(echo "$fbar_value" | grep '^\{')
+    if [ "$fbar_check_section" != "" ];then
+        fbar_value=$(fbfr_section "$fbar_file" "$fbar_key" "{" "}")
+        if [ "$fbar_value" == "" ];then
+            return 0
+        else
+            echo "$fbar_value"
+            return 2
+        fi
+    fi
+    fbar_check_section=$(echo "$fbar_value" | grep '^\[')
+    if [ "$fbar_check_section" != "" ];then
+        fbar_value=$(fbfr_section "$fbar_file" "$fbar_key" "[" "]")
+        if [ "$fbar_value" == "" ];then
+            return 0
+        else
+            fbar_command=$(echo "$fbar_value" | sed -e '1s/^\[//' | sed -e '$s/]$//')
+            fbar_value=$(fbfr_parse_hook "$fbar_command" "$fbar_temp_path")
+            echo "$fbar_value"
+            return 3
+        fi
+    fi
+    echo "$fbar_value"
+    return 1
 }
 
 
