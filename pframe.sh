248584c4724d2abed2f2cbe06b47d463680f02d1
zhijli <307999258@qq.com>
2022-07-10 11:00:44 +0800

    pframe增加-i选项

--- pframe.sh
+++ pframe.sh
@@ -11,29 +11,27 @@ export FBAU_PF_SCRIPTS="${FBAU_PF_ROOT}/scripts"
 export FBAU_PF_BIN="${FBAU_PF_ROOT}/bin"
 fbar_plugin_path="${FBAU_PF_ROOT}/plugins"
 
+. ${FBAU_PF_SCRIPTS}/base.sh
+
 if [ "$PATH" == "" ];then
     export PATH="${FBAU_PF_BIN}"
 else
     export PATH="${PATH}:${FBAU_PF_BIN}"
 fi
 
-. ${FBAU_PF_SCRIPTS}/base.sh
-
-#初始化pframe环境
-${FBAU_PF_ROOT}/init/init.sh
-if [ "$?" != "0" ];then
-    fbfu_error "$0: environment init failed"
-    exit 1
-fi
-
 fbar_work=$(pwd)
-
-. ${FBAU_PF_SCRIPTS}/jshn.sh
-. ${FBAU_PF_SCRIPTS}/parser.sh
-
 fbar_usage="Usage: $0 [ -f custom config ] [ -p plugin ] [ -t temp directory ]"
 
-while getopts :f:p:t: opt
+function init_pf_env {
+    #初始化pframe环境
+    ${FBAU_PF_ROOT}/init/init.sh
+    if [ "$?" != "0" ];then
+        fbfu_error "$0: environment init failed"
+        exit 1
+    fi
+}
+
+while getopts :f:p:t:i opt
 do
     case "$opt" in
         f) 
@@ -46,12 +44,18 @@ do
         t)
             export FBAU_TEMP_DIR=$(fbfu_convert_relative_path "$fbar_work" "$OPTARG")
             ;;
+        i)
+            init_pf_env
+            exit 0
+            ;;
         *) 
             echo "$fbar_usage"
             exit 1
             ;;
     esac
 done
+
+init_pf_env
 
 if [ ! -f "$FBAU_CUSTOM_CONFIG" ];then
     fbfu_error "$0: custom config [${FBAU_CUSTOM_CONFIG}] not found"
