aae13b0865d18007d3a487665b3f3bc21d97f1e4
zhijli <307999258@qq.com>
2022-07-12 22:40:17 +0800

    增加template，简化逻辑

--- build.sh
+++ build.sh
@@ -29,11 +29,15 @@ fi
 . ${FBAU_SCRIPTS}/fbc.sh
 
 fbar_node_suffix="fwb.n"
+fbar_template_suffix="tl.sh"
+fbar_module_suffix="ml.sh"
 fbar_build_dir="${FBAU_PROJECT}/build"
 fbar_temp_dir="${fbar_build_dir}/tmp"
 fbar_hook="${fbar_build_dir}/tmp/__hook_sh"
 fbar_main_node="${FBAU_PROJECT}/${fbar_node_suffix}"
 fbar_nodes_dir="${FBAU_PROJECT}/nodes"
+fbar_modules_dir="${fbar_root}/modules"
+fbar_templates_dir="${fbar_root}/templates"
 fbar_node_temp="${fbar_temp_dir}/node"
 fbar_node_chain="${fbar_node_temp}/chain"
 
@@ -48,6 +52,8 @@ function fbfr_fbc_init {
     FBAR_FBC_END=0
     fbfu_kvlist_init "FBAR_FBC_LIST" "FBAR_FBC_LIST_SUM" "FBAR_FBC_END"
     fbfu_fbc_set "NPATH_BASE" "$fbar_nodes_dir"
+    fbfu_fbc_set "MODULES_BASE" "$fbar_modules_dir"
+    fbfu_fbc_set "TEMPLATES_BASE" "$fbar_templates_dir"
 }
 
 fbfr_fbc_init
@@ -77,7 +83,6 @@ function fbfr_gen_hook {
 function fbfr_handle_node {
     local fbar_m=0
     local fbar_value=""
-    local fbar_result=0
     local fbar_next_node=$1
     local fbar_custom_path=${fbar_next_node%/*}
     local fbar_node_name=${fbar_custom_path##*/}
@@ -89,46 +94,27 @@ function fbfr_handle_node {
     fbar_value=$(fbfu_parse "$fbar_next_node" "TRACE_ON" "$fbar_temp_dir")
     if [ "$?" == "4" ];then
         fbfr_gen_hook "$fbar_value"
-        . $fbar_hook "START"
-        fbar_result="$?"
+        . $fbar_hook "$fbar_custom_path" "START"
     fi
     touch "${fbar_node_chain}/${fbar_node_name}"
-#错误码:11表示异常
-    if [ "$fbar_result" == "11" ];then
-        fbar_value=$(fbfu_parse "$fbar_next_node" "TRACE_OFF" "$fbar_temp_dir")
-        if [ "$?" == "4" ];then
-            fbfr_gen_hook "$fbar_value"
-            . $fbar_hook  "$fbar_custom_path" "EXCEPTION"
+    FBAR_CURRENT_NODE="$fbar_next_node"
+    if [ "$FBAR_TEMPLATE" != "" ];then
+        if [ "$fbar_next_node" == "$fbar_main_node" ];then
+            fbar_tl_king
+        else
+            fbar_tl_attendant
         fi
-        return 1
     fi
-    local fbar_node_depend=$(fbfu_parse "$fbar_next_node" "DEPEND" "$fbar_temp_dir")
-    if [ "$fbar_node_depend" == "" ];then
-        fbar_value=$(fbfu_parse "$fbar_next_node" "TRACE_OFF" "$fbar_temp_dir")
-        if [ "$?" == "4" ];then
-            fbfr_gen_hook "$fbar_value"
-            . $fbar_hook "$fbar_custom_path" "STOP"
-        fi
-        return 0
-    fi
-    local fbar_node_depend_array=($fbar_node_depend)
-    local fbar_rdsize=${#fbar_node_depend_array[@]}
-    fbfu_info "NODE: ${fbar_node_name} --> ${fbar_node_depend_array[@]}"
+    local fbar_node_depend=($(fbfu_parse "$fbar_next_node" "DEPEND" "$fbar_temp_dir"))
+    local fbar_rdsize=${#fbar_node_depend[@]}
+    fbfu_info "NODE: ${fbar_node_name} --> ${fbar_node_depend[@]}"
     while [ "$fbar_m" -lt "$fbar_rdsize" ];do
-        local fbar_next_depend=${fbar_node_depend_array[$fbar_m]}
+        local fbar_next_depend=${fbar_node_depend[$fbar_m]}
         local fbar_next_depend_node=$(fbfu_fbc_foreach "fbfr_node_search" "$fbar_next_depend")
         if [ "$fbar_next_depend_node" == "" ];then
             fbfu_warn "NODE: ${fbar_next_depend} not exist"
         else
             fbfr_handle_node "$fbar_next_depend_node"
-            if [ "$?" == "1" ];then
-                fbar_value=$(fbfu_parse "$fbar_next_node" "TRACE_OFF" "$fbar_temp_dir")
-                 if [ "$?" == "4" ];then
-                    fbfr_gen_hook "$fbar_value"
-                    . $fbar_hook "$fbar_custom_path" "FORCE"
-                fi
-                return 1
-            fi
         fi
         fbar_m=$[ "$fbar_m" + 1 ]
     done
@@ -140,15 +126,41 @@ function fbfr_handle_node {
     return 0
 }
 
+function fbfr_template_search {
+    local fbar_check_key=$(echo "$1" | grep "^TEMPLATES_")
+    if [ "$fbar_check_key" != "" ];then
+        local fbar_search_template="$2/$4"
+        local fbar_template_sh="${fbar_search_template}/${fbar_template_suffix}"
+        if [ -f "${fbar_template_sh}" ];then
+            . $fbar_template_sh
+            FBAR_TEMPLATE="$fbar_search_template"
+            return 1
+        else
+            return 0
+        fi
+    else
+        return 0
+    fi
+}
+
+function fbfr_import_template {
+    local fbar_value=$(fbfu_parse "$fbar_main_node" "TEMPLATE" "$fbar_temp_dir")
+    if [ "$fbar_value" == "" ];then
+        return
+    fi
+    fbfu_fbc_foreach "fbfr_template_search" "$fbar_value"
+}
+
 function fbfr_node_frame {
     if [ -d "$fbar_node_temp" ];then
         rm -rf "$fbar_node_temp"
         if [ "$?" != "0" ];then
-            fbfu_error "remove $fbar_node_temp error"
+            fbfu_error "remove ${fbar_node_temp} error"
             exit 1
         fi
     fi
     mkdir -p $fbar_node_chain
+    fbfr_import_template
     fbfr_handle_node "$fbar_main_node"
 }
 
