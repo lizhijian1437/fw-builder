e795962e21bd617cd89c08c19f7f4052b6b88824
zhijli <307999258@qq.com>
2022-07-18 19:35:59 +0800

    统一异常打印格式

--- modules/ipk_build/ml.sh
+++ modules/ipk_build/ml.sh
@@ -15,12 +15,13 @@ fbar_ipk_build="${fbar_module_path}/ipkg-build"
 
 mkdir -p $fbar_temp_path 1>/dev/null 2>&1
 if [ ! -f "$fbar_config" ] || [ ! -d "$fbar_temp_path" ];then
-    fbfu_error "[IPK] args error"
+    fbfu_error "[IPK]args error"
     exit 1
 fi
 
 fbar_root=$(fbfu_parse "$fbar_config" "IPK_PACKAGE_ROOT" "$fbar_temp_path")
 if [ ! -d "$fbar_root" ];then
+    fbfu_error "[IPK]please set IPK_PACKAGE_ROOT"
     exit 1
 fi
 fbar_control="${fbar_root}/CONTROL/control"
@@ -28,29 +29,29 @@ fbar_control="${fbar_root}/CONTROL/control"
 fbar_out=$(fbfu_parse "$fbar_config" "IPK_PACKAGE_OUT" "$fbar_temp_path")
 mkdir -p $fbar_out 1>/dev/null 2>&1
 if [ ! -d "$fbar_out" ];then
-    fbfu_error "[IPK] opkg out not exist"
+    fbfu_error "[IPK]please set IPK_PACKAGE_OUT"
     exit 1
 fi
 
 if [ ! -f "$fbar_control" ];then
     fbfu_force_touch "$fbar_control"
     if [ ! -f "$fbar_control" ];then
-        fbfu_error "[IPK] can't create control file"
+        fbfu_error "[IPK]can't create control file"
         exit 1
     fi
     fbar_package_name=$(fbfu_parse "$fbar_config" "IPK_PACKAGE_NAME" "$fbar_temp_path")
     if [ "$fbar_package_name" == "" ];then
-        fbfu_error "[IPK] please provide package name"
+        fbfu_error "[IPK]please provide package name"
         exit 1
     fi
     fbar_package_version=$(fbfu_parse "$fbar_config" "IPK_PACKAGE_VERSION" "$fbar_temp_path")
     if [ "$fbar_package_version" == "" ];then
-        fbfu_error "[IPK] please provide package version"
+        fbfu_error "[IPK]please provide package version"
         exit 1
     fi
     fbar_arch=$(fbfu_parse "$fbar_config" "IPK_PACKAGE_ARCH" "$fbar_temp_path")
     if [ "$fbar_arch" == "" ];then
-        fbfu_error "[IPK] please provide package arch"
+        fbfu_error "[IPK]please provide package arch"
         exit 1
     fi
     chmod 755 $fbar_control 1>/dev/null 2>&1
@@ -68,19 +69,19 @@ function fbar_required_field
 
 fbar_package_name=$(fbar_required_field "Package")
 if [ "$fbar_package_name" == "" ];then
-    fbfu_error "[IPK] please provide package name"
+    fbfu_error "[IPK]please provide package name"
     exit 1
 fi
 
 fbar_package_version=$(fbar_required_field "Version")
 if [ "$fbar_package_version" == "" ];then
-    fbfu_error "[IPK] please provide package version"
+    fbfu_error "[IPK]please provide package version"
     exit 1
 fi
 
 fbar_arch=$(fbar_required_field "Architecture")
 if [ "$fbar_arch" == "" ];then
-    fbfu_error "[IPK] please provide package arch"
+    fbfu_error "[IPK]please provide package arch"
     exit 1
 fi
 
