b044588bada3b6f03899abf7dfdc0b49dde93e48
zhijli <307999258@qq.com>
2022-07-19 22:37:01 +0800

    firmware模版增加ipk包的安装和删除

--- templates/firmware/tl.sh
+++ templates/firmware/tl.sh
@@ -16,6 +16,7 @@ function fbar_king_init {
     export FBAU_NODE_BUILD_DIR="${fbar_template_build}/${FBAU_CURRENT_NODE_NAME}"
     export FBAU_PACKAGE_OUT="${FBAU_NODE_BUILD_DIR}/out"
     export FBAU_ROOTFS_ROOT="${FBAU_NODE_BUILD_DIR}/rootfs"
+    export FBAU_IPK_INSTALL_DIR="${FBAU_NODE_BUILD_DIR}/ipk_install"
     if [ -d "$FBAU_PACKAGE_OUT" ];then
         rm -rf $FBAU_PACKAGE_OUT
     fi
@@ -24,39 +25,51 @@ function fbar_king_init {
         rm -rf $FBAU_ROOTFS_ROOT
     fi
     mkdir -p $FBAU_ROOTFS_ROOT
+    if [ -d "$FBAU_IPK_INSTALL_DIR" ];then
+        rm -rf $FBAU_IPK_INSTALL_DIR
+    fi
+    mkdir -p $FBAU_IPK_INSTALL_DIR
 }
 
 function fbar_tl_king {
     if [ "$1" == "INIT" ];then
         fbar_king_init
+    elif [ "$1" == "START" ];then
+        if [ "$FBAU_DEFAULT_ARCH" == "" ];then
+            fbfr_search_arch
+        fi
+        fbfu_fbc_module "opkg_rmpkg" "${FBAR_TEMPLATE}/config/opkg.n"
+        fbfu_fbc_module "opkg_install" "${FBAR_TEMPLATE}/config/opkg.n"
     fi
 }
 
 function fbar_attendant_env {
     export FBAU_NODE_BUILD_DIR="${fbar_template_build}/${FBAU_CURRENT_NODE_NAME}"
-    export FBAU_IPK_DIR="${FBAU_NODE_BUILD_DIR}/ipk"
+    export FBAU_IPK_WORKDIR="${FBAU_NODE_BUILD_DIR}/ipk"
+    export FBAU_IPK_ROOT="${FBAU_IPK_WORKDIR}/ipk_build"
     if [ "$FBAU_DEFAULT_ARCH" == "" ];then
         fbfr_search_arch
     fi
-    local fbar_ipk_out="${FBAU_IPK_DIR}/__ipk_out"
-    local fbar_out=$(fbfu_fbc_parse "IPK_PACKAGE_OUT" "${FBAR_TEMPLATE}/config/ipk-build.n")
+    local fbar_ipk_out="${FBAU_IPK_WORKDIR}/ipk_out"
+    local fbar_output=$(fbfu_fbc_parse "IPK_PACKAGE_OUT" "${FBAR_TEMPLATE}/config/ipk-build.n")
     local fbar_cache=$(fbfu_fbc_parse "IPK_CACHE")
     fbar_ipk_rebuild="true"
-    if [ "$fbar_cache" == "true" ] && [ -f "${fbar_ipk_out}/__ipk_build_ok" ];then
+    if [ "$fbar_cache" == "true" ] && [ -f "${fbar_ipk_out}/ipk_build_ok" ];then
         fbar_ipk_rebuild="false"
-        mkdir -p "$fbar_out"
-        if [ -d "$fbar_out" ];then
-            cp ${fbar_ipk_out}/*.ipk "$fbar_out"
+        mkdir -p "$fbar_output"
+        if [ -d "$fbar_output" ];then
+            cp ${fbar_ipk_out}/*.ipk "$fbar_output"
             fbfu_info "[${FBAU_CURRENT_NODE_NAME}]CACHE PACKAGE"
         else
-            fbfu_warn "[${FBAU_CURRENT_NODE_NAME}]${fbar_out} not exist"
+            fbfu_warn "[${FBAU_CURRENT_NODE_NAME}]${fbar_output} not exist"
         fi
     else
-        if [ -d "$FBAU_IPK_DIR" ];then
-            rm -rf $FBAU_IPK_DIR
+        if [ -d "$FBAU_IPK_WORKDIR" ];then
+            rm -rf $FBAU_IPK_WORKDIR
         fi
     fi
-    mkdir -p $FBAU_IPK_DIR
+    mkdir -p $FBAU_IPK_WORKDIR
+    mkdir -p $FBAU_IPK_ROOT
 }
 
 function fbar_tl_attendant {
