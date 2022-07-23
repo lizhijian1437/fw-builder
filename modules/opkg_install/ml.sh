0af2574206b901121f29433faddc5cd97cea0aef
zhijli <307999258@qq.com>
2022-07-23 16:58:29 +0800

    修改opkg install的内部打印

--- modules/opkg_install/ml.sh
+++ modules/opkg_install/ml.sh
@@ -41,9 +41,10 @@ fi
 
 function __fbar_install_package {
     local fbar_file=$1
+    local fbar_file_name=${fbar_file##*/}
     local fbar_file_suffix=${fbar_file##*.}
     if [ "$fbar_file_suffix" == "ipk" ];then
-        fbfu_info "[OPKG_INSTALL]install $fbar_file"
+        fbfu_info "[OPKG_INSTALL]install $fbar_file_name"
         $fbar_opkg -o "$fbar_opkg_root" -add-arch all:1 -add-arch noarch:1 -add-arch "$fbar_opkg_arch:100" install "$fbar_file" --nodeps
     fi
     return 0
