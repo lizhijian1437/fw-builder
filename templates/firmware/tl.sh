d4269b1a9d7a84cd30a88c440035cd781c52e58f
zhijli <307999258@qq.com>
2022-07-23 15:37:52 +0800

    firmware模块按照阶段进行划分

--- templates/firmware/tl.sh
+++ templates/firmware/tl.sh
@@ -7,51 +7,87 @@ function fbfr_search_arch {
     if [ -f "$fbar_release_file" ];then
         FBAU_DEFAULT_ARCH=$(fbfu_parse_kv "$fbar_release_file" "DISTRIB_ARCH" "=" | sed -e "s/'//g")
     fi
-    if [ "$FBAU_DEFAULT_ARCH" == "" ];then
-        fbfu_warn "unKnown arch"
+    if [ "$FBAU_DEFAULT_ARCH" != "" ];then
+        fbfu_warn "DEFAULT_ARCH:${FBAU_DEFAULT_ARCH}"
     fi
 }
 
-function fbar_king_init {
-    local fbar_toolchains_dir="${FBAU_PROJECT}/toolchains"
-    fbfu_fbc_set "TOOLCHAIN_BASE" "$fbar_toolchains_dir"
-    export FBAU_NODE_BUILD_DIR="${fbar_template_build}/${FBAU_CURRENT_NODE_NAME}"
-    export FBAU_PACKAGE_OUT="${FBAU_NODE_BUILD_DIR}/out"
-    export FBAU_ROOTFS_ROOT="${FBAU_NODE_BUILD_DIR}/rootfs"
-    export FBAU_IPK_INSTALL_DIR="${FBAU_NODE_BUILD_DIR}/ipk_install"
+function fbfr_FW_BUILD {
+    local fbar_value="$1"
     local fbar_partition=$(fbfu_fbc_parse "FIRMWARE_OUTPUT" "${FBAR_TEMPLATE}/config/fw_build.n")
     if [ -f "$fbar_partition" ];then
         rm -rf "$fbar_partition"
     fi
-    if [ -d "$FBAU_PACKAGE_OUT" ];then
-        rm -rf $FBAU_PACKAGE_OUT
+    if [ "$fbar_value" != "" ];then
+        fbfu_fbc_gen_hook "$fbar_value"
+        . $FBAU_HOOK "FW_BUILD_BEGIN"
     fi
-    mkdir -p $FBAU_PACKAGE_OUT
-    if [ -d "$FBAU_ROOTFS_ROOT" ];then
-        rm -rf $FBAU_ROOTFS_ROOT
+    fbfu_fbc_module "fw_build" "${FBAR_TEMPLATE}/config/fw_build.n"
+    if [ "$fbar_value" != "" ];then
+        fbfu_fbc_gen_hook "$fbar_value"
+        . $FBAU_HOOK "FW_BUILD_FINISH"
     fi
-    mkdir -p $FBAU_ROOTFS_ROOT
-    if [ -d "$FBAU_IPK_INSTALL_DIR" ];then
-        rm -rf $FBAU_IPK_INSTALL_DIR
-    fi
-    mkdir -p $FBAU_IPK_INSTALL_DIR
 }
 
-function fbar_king_env {
-    export FBAU_NODE_BUILD_DIR="${fbar_template_build}/${FBAU_CURRENT_NODE_NAME}"
+function fbfr_OPKG_INSTALL {
+    local fbar_value="$1"
+    if [ "$fbar_value" != "" ];then
+        fbfu_fbc_gen_hook "$fbar_value"
+        . $FBAU_HOOK "OPKG_INSTALL_BEGIN"
+    fi
+    fbfu_fbc_module "opkg_install" "${FBAR_TEMPLATE}/config/opkg.n"
+    if [ "$fbar_value" != "" ];then
+        fbfu_fbc_gen_hook "$fbar_value"
+        . $FBAU_HOOK "OPKG_INSTALL_FINISH"
+    fi
 }
 
-function fbar_tl_king {
+function fbfr_OPKG_RMPKG {
+    local fbar_value="$1"
+    if [ "$fbar_value" != "" ];then
+        fbfu_fbc_gen_hook "$fbar_value"
+        . $FBAU_HOOK "OPKG_RMPKG_BEGIN"
+    fi
+    fbfu_fbc_module "opkg_rmpkg" "${FBAR_TEMPLATE}/config/opkg.n"
+    if [ "$fbar_value" != "" ];then
+        fbfu_fbc_gen_hook "$fbar_value"
+        . $FBAU_HOOK "OPKG_RMPKG_FINISH"
+    fi
+}
+
+function fbfr_tl_king {
     if [ "$1" == "IN" ];then
-        fbar_king_init
+        local fbar_value=""
+        export FBAU_NODE_BUILD_DIR="${fbar_template_build}/${FBAU_CURRENT_NODE_NAME}"
+        export FBAU_PACKAGE_OUT="${FBAU_NODE_BUILD_DIR}/out"
+        export FBAU_ROOTFS_ROOT="${FBAU_NODE_BUILD_DIR}/rootfs"
+        export FBAU_IPK_INSTALL_DIR="${FBAU_NODE_BUILD_DIR}/ipk_install"
+        fbfu_fbc_set "TOOLCHAIN_BASE" "${FBAU_PROJECT}/toolchains"
+        if [ -d "$FBAU_PACKAGE_OUT" ];then
+            rm -rf $FBAU_PACKAGE_OUT
+        fi
+        if [ -d "$FBAU_ROOTFS_ROOT" ];then
+            rm -rf $FBAU_ROOTFS_ROOT
+        fi
+        if [ -d "$FBAU_IPK_INSTALL_DIR" ];then
+            rm -rf $FBAU_IPK_INSTALL_DIR
+        fi
+        mkdir -p $FBAU_PACKAGE_OUT
+        mkdir -p $FBAU_ROOTFS_ROOT
+        mkdir -p $FBAU_IPK_INSTALL_DIR
     elif [ "$1" == "OUT" ];then
-        fbar_king_env
+        export FBAU_NODE_BUILD_DIR="${fbar_template_build}/${FBAU_CURRENT_NODE_NAME}"
+#搜索ARCH
         if [ "$FBAU_DEFAULT_ARCH" == "" ];then
             fbfr_search_arch
         fi
-        fbfu_fbc_module "opkg_rmpkg" "${FBAR_TEMPLATE}/config/opkg.n"
-        fbfu_fbc_module "opkg_install" "${FBAR_TEMPLATE}/config/opkg.n"
-        fbfu_fbc_module "fw_build" "${FBAR_TEMPLATE}/config/fw_build.n"
+        fbar_value=$(fbfu_fbc_parse "TRACE")
+#OPKG_RMPKG阶段
+        fbfr_OPKG_RMPKG "$fbar_value"
+#OPKG_INSTALL阶段
+        fbfr_OPKG_INSTALL "$fbar_value"
+#FW_BUILD阶段
+        fbfr_FW_BUILD "$fbar_value"
     fi
 }
 
@@ -71,14 +107,7 @@ function __fbfr_search_toolchain {
     fi
 }
 
-function fbar_attendant_env {
-    local fbar_toolchain=$(fbfu_fbc_parse "TOOLCHAIN")
-    export FBAU_NODE_BUILD_DIR="${fbar_template_build}/${FBAU_CURRENT_NODE_NAME}"
-    export FBAU_IPK_WORKDIR="${FBAU_NODE_BUILD_DIR}/ipk"
-    export FBAU_IPK_ROOT="${FBAU_IPK_WORKDIR}/ipk_build"
-    if [ "$FBAU_DEFAULT_ARCH" == "" ];then
-        fbfr_search_arch
-    fi
+function fbfr_IPK_BUILD_clean {
     local fbar_ipk_out="${FBAU_IPK_WORKDIR}/ipk_out"
     local fbar_output=$(fbfu_fbc_parse "IPK_PACKAGE_OUT" "${FBAR_TEMPLATE}/config/ipk-build.n")
     local fbar_cache=$(fbfu_fbc_parse "IPK_CACHE")
@@ -97,19 +126,43 @@ function fbar_attendant_env {
             rm -rf $FBAU_IPK_WORKDIR
         fi
     fi
-    mkdir -p $FBAU_IPK_WORKDIR
     mkdir -p $FBAU_IPK_ROOT
-    if [ "$fbar_toolchain" != "" ];then
-        fbfu_fbc_foreach " __fbfr_search_toolchain" "$fbar_toolchain"
+}
+
+function fbfr_IPK_BUILD {
+    local fbar_value="$1"
+    fbfr_IPK_BUILD_clean
+    if [ "$fbar_value" != "" ];then
+        fbfu_fbc_gen_hook "$fbar_value"
+        . $FBAU_HOOK "IPK_BUILD_BEGIN"
+    fi
+    if [ "$fbar_ipk_rebuild" == "true" ];then
+        fbfu_info "[${FBAU_CURRENT_NODE_NAME}]BUILD PACKAGE"
+        fbfu_fbc_module "ipk_build" "${FBAR_TEMPLATE}/config/ipk-build.n"
+    fi
+    if [ "$fbar_value" != "" ];then
+        fbfu_fbc_gen_hook "$fbar_value"
+        . $FBAU_HOOK "IPK_BUILD_FINISH"
     fi
 }
 
-function fbar_tl_attendant {
+function fbfr_tl_attendant {
     if [ "$1" == "OUT" ];then
-        fbar_attendant_env
-        if [ "$fbar_ipk_rebuild" == "true" ];then
-            fbfu_info "[${FBAU_CURRENT_NODE_NAME}]BUILD PACKAGE"
-            fbfu_fbc_module "ipk_build" "${FBAR_TEMPLATE}/config/ipk-build.n"
+        local fbar_value=""
+        local fbar_toolchain=$(fbfu_fbc_parse "TOOLCHAIN")
+        export FBAU_NODE_BUILD_DIR="${fbar_template_build}/${FBAU_CURRENT_NODE_NAME}"
+        export FBAU_IPK_WORKDIR="${FBAU_NODE_BUILD_DIR}/ipk"
+        export FBAU_IPK_ROOT="${FBAU_IPK_WORKDIR}/ipk_build"
+#搜索ARCH
+        if [ "$FBAU_DEFAULT_ARCH" == "" ];then
+            fbfr_search_arch
         fi
+#搜索工具链
+        if [ "$fbar_toolchain" != "" ];then
+            fbfu_fbc_foreach " __fbfr_search_toolchain" "$fbar_toolchain"
+        fi
+#IPK_BUILD阶段
+        fbar_value=$(fbfu_fbc_parse "TRACE")
+        fbfr_IPK_BUILD "$fbar_value"
     fi
 }
